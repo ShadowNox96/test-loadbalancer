cdf137eec5836b58c91d90d2d77dfae4f807dfcb
Emanuel de la Cruz <cdcarlose32@gmail.com>
2022-07-02 13:45:01 -0600

    nodo1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index 
-    @user = params[:user]
-    puts '==========================USUARIO ============'
-    puts params
-    flash[:alert] = "USER: #{@user}"
   end 
 
   def print
